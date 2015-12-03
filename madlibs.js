9a84e8f4fb908a6446ebec3a41a749b87b660aa8
keananpierre1234 <keananpierrekj@hotmail.com>
2015-12-03 22:10:11 +0000

    8 fields in madlibs are working

--- madlibs.js
+++ madlibs.js
@@ -1,7 +1,7 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = "Welcome to ScriptEd! Our mascot is the PLURAL_NOUN.  We love to VERB! ";
+  var story = "Welcome to ScriptEd! Our mascot is the PLURAL_NOUN.  We love to VERB! We love to VERB-1! ";
   
 
   var inputValue = document.getElementById("plural-noun-1").value;
@@ -10,6 +10,19 @@ function generateMadlib() {
   inputValue = document.getElementById("verb").value;
   story = story.replace("VERB", inputValue);
   
+  inputValue = document.getElementById("verb-1").value;
+  story = story.replace("VERB-1", inputValue);
+  
+ inputValue = document.getElementbyId("place").value;
+  story = story.replace("place", inputValue);
+  
+ inputValue = document.getElementbyId("noun").value;
+ story = story.replace("noun", inputValue);
+ 
+ inputValue = document.getElementbyId("verb").value;
+ 
+ 
+ 
   // This writes the story when the user clicks the "Generate MadLib" button"
   document.getElementById("result").innerHTML = story;
 }
